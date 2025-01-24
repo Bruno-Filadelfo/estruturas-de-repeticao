78980368e6661e38af2114d5ce11df862f112117
Bruno Filadelfo <Bruno.Filadelfo@Outlook.Com>
2025-01-24 01:39:12 -0300

    fluxo de execução while , break

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int numero = 5;
+int contador = 1;
+
+while (contador <= 10) {
+    Console.WriteLine($"{contador}° execução: {numero} x {contador} = {numero * contador}");
+    contador++;
+
+    if (contador == 6) {
+        break;
+    }
+}
